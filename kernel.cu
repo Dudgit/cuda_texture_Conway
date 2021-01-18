0a4c12ed28a40fa28d47e41e8411182fa57a8b9f
Dudgit <52243953+Dudgit@users.noreply.github.com>
2021-01-18 14:28:51 +0100

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -6,83 +6,73 @@
 #include "texture_fetch_functions.h"
 #include "texture_types.h"
 
+texture<float, 1, cudaReadModeElementType> texInput;
 
 
-
-__global__ void texture_c(int* output, cudaTextureObject_t input)
+__global__ void texture_c(float* output)
 {
 	/*
 	Inputnak próbálkoztam int1 típussal is valamint int array-el, ugyan az a végeredmény
 	*/
 	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-	output[y * h + x] = tex2D<int>(input, x, y);
+	//output[y * h + x] = tex2D<float>(input, x, y);
+	output[y * h + x] = tex1Dfetch<float>(texInput, y * h + x);
+	
+	
+	
 }
 
 int main()
 {
 	// Creating the Vectors
-	std::vector<bool> bools(w*h); 
+	std::vector<bool> bools(w * h);
 	initializer(bools);
 
-	std::vector<int> int_vector(w*h);
-	std::vector<int> output(h*w);
+	std::vector<float> int_vector(w * h);
+	std::vector<float> output(h * w);
 	initializer(int_vector);
 
+	float* hInput = (float*)malloc(sizeof(float) *	h*w);
+	float* hOutput = (float*)malloc(sizeof(float) * h*w);
+	for (int i = 0; i < h*w; i++)
+	{
+		hInput[i] = int_vector[i];
+	}
 
-	//Habár itt integereket használok, ha átírom a Floatot nem mûködik
-	cudaChannelFormatDesc channelDescInput = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
-	cudaArray* aInput;
 
-	cudaError_t err = cudaMallocArray(&aInput, &channelDescInput, w, h);
+	float* dInput = NULL, * dOutput = NULL;
+
+	size_t offset = 0;
+
+	texInput.addressMode[0] = cudaAddressModeBorder;
+	texInput.addressMode[1] = cudaAddressModeBorder;
+	texInput.filterMode = cudaFilterModePoint;
+	texInput.normalized = false;
+
+
+	cudaError_t err = (cudaMalloc((void**)&dInput, sizeof(float) *h*w));
 	if (err != cudaSuccess) { std::cout << "Error allocating Cuda memory: " << cudaGetErrorString(err) << '\n'; return -1; }
-	
-	
-	err = cudaMemcpyToArray(aInput, 0, 0, int_vector.data(), w * h * sizeof(int), cudaMemcpyHostToDevice);
-	if (err != cudaSuccess) { std::cout << "Error copying memory to device: " << cudaGetErrorString(err) << '\n'; return -1; }
-	
-	cudaResourceDesc resdescInput{};
-	resdescInput.resType = cudaResourceTypeArray;
-	resdescInput.res.array.array = aInput;
 
-	cudaTextureDesc texD{};
-	texD.addressMode[0] = cudaAddressModeBorder;
-	texD.addressMode[1] = cudaAddressModeBorder;
-	texD.filterMode = cudaFilterModeLinear;
-	texD.readMode = cudaReadModeElementType;
-	texD.normalizedCoords = 0;
+	err = (cudaMalloc((void**)&dOutput, sizeof(float) * h * w));
+	if (err != cudaSuccess) { std::cout << "Error allocating Cuda memory: " << cudaGetErrorString(err) << '\n'; return -1; }
 
+	cudaMemcpy(dInput, hInput, sizeof(float) * h*w, cudaMemcpyHostToDevice);
 
-	cudaTextureObject_t t_handler = 0;
-	err = cudaCreateTextureObject(&t_handler, &resdescInput, &texD, nullptr);
-	if (err != cudaSuccess) { std::cout << "Error creating texture object : " << cudaGetErrorString(err) << '\n'; return -1; }
+	cudaBindTexture(&offset, texInput, dInput, sizeof(float) * h*w);
 
-
-	int* c_output;
-	//int* c_output = new int[h*w];
-	err = cudaMalloc((void**)&c_output, w * h * sizeof(int));
-	if (err != cudaSuccess) { std::cout << "Error allocating CUDA memory: " << cudaGetErrorString(err) << '\n'; return -1; }
-	
 
 
 	dim3 dimGrid(w / block_size, h / block_size);
 	dim3 dimBlock(block_size, block_size);
 
-	texture_c <<< dimGrid,  dimBlock>>> (c_output, t_handler);
+	texture_c <<< dimGrid, dimBlock >>> (dOutput);
 
 	//std::cout << "Succes rages on" << std::endl;
-	err = cudaMemcpy(output.data(),c_output, w * h * sizeof(int), cudaMemcpyDeviceToHost);
+	err = cudaMemcpy(hOutput, dOutput, w * h * sizeof(float), cudaMemcpyDeviceToHost);
 	if (err != cudaSuccess) { std::cout << "Error copying memory to host: " << cudaGetErrorString(err) << "\n"; return -1; }
-	for (auto e:output)
+	for (int i = 0; i < h * w; ++i)
 	{
-		  std::cout << e << std::endl; 
+		std::cout << hOutput[i] << " " << int_vector[i] << std::endl;
 	}
-
-	/*
-	table f_table(w,h,bs);
-	f_table.write_table_out();
-	std::cout << std::endl;
-	step(f_table);
-	f_table.write_table_out();
-	*/
-}	
+}
