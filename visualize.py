abf1130a872917d22c18049c96c52b0baa7243d6
Dudgit <52243953+Dudgit@users.noreply.github.com>
2021-01-25 20:39:11 +0100

    Add files via upload

--- visualize.py
+++ visualize.py
@@ -1,8 +1,10 @@
+
+#%%
 import pandas as  pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+#%%
 handler = np.loadtxt('data/cw1.csv')
 
 def save_anim(h,num):
@@ -11,12 +13,13 @@ def save_anim(h,num):
     plt.title("Conway", fontsize = 20)
     im = plt.imshow(mD[0])
     plt.show()
-
+  
     def animate(i):
         im.set_array(mD[i])
         return [im]
 
-    anim = animation.FuncAnimation(fig,animate,frames = 10 ,interval = 10000 ) # in ms)
-    anim.save("Cw_game_"+str(num)+ ".mp4")
+    anim = animation.FuncAnimation(fig,animate,frames = 1000 ,interval = 100 )
+    anim.save("Cw_game_"+str(num)+ ".gif",writer='pillow')
     
-    save_anim(handler,1)
+save_anim(handler,1)
+# %%
